3104921915cadd3472d7259d412ecc70b4832cf4
MakerMax Inc <43316514+makermaxinc@users.noreply.github.com>
2020-02-18 12:09:00 +0530

    Update Makermax_Ion.ino
    Added support for buttons on the board and tutorials.

--- Arduino/Makermax_Ion/Makermax_Ion.ino
+++ Arduino/Makermax_Ion/Makermax_Ion.ino
@@ -1,14 +1,41 @@
+//Libraries
 #include <mcp47FEB22.h>
-
 #include <MCP342x.h>
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
 
+// Guidelines:
+//
+//  MODE 0 - IDLE STATE
+//  MODE 1 - CHARGING
+//  MODE 2 - DISCHARGING
+//
+//  1. Press and hold SW1 & SW2 for 1 second for Mode to change.
+//  2. Press and hold SW1 for 1 second for Decrement in charging/discharging Current by 50mA after entering Mode1 or Mode2.
+//  3. Press and hold SW2 for 1 second for Increment in charging/discharging Current by 50mA after entering Mode1 or Mode2.
+
+
+
+//Global declarations
+
+bool controlLock = false;
+uint8_t red_flag = 0;  //Guard against voltage dropping below 2.8
 uint8_t address = 0x68;
+
 MCP342x adc = MCP342x(address);
 mcp47FEB22 dac(0x60);
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
+uint8_t mode = 0; // 0 - idle, 1 charge, 2 dischage
+float chg_curr  = 0;
+float dchg_curr = 0;
+
+//(discharge current value(mA),charge current value(mA))
+ float CHARGE     =    500;                            //Initial charge value
+ float DISCHARGE  =    500;                            //Initial Discharge value
+
+#define UNDER_VOLTAGE   2.80
+#define OVER_VOLTAGE    4.20     
+#define SCREEN_WIDTH  128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
@@ -18,27 +45,34 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 float cshunt=0.2;
 float dshunt=0.2;
 
-void setup() {
-dac.begin();
 
-   Serial.begin(9600);
-Wire.begin();
+
+
+
+//Initializations
+void setup() {
+dac.begin();          //Initializes DAC
+
+Serial.begin(9600);
+Wire.begin();       //Initializes I2C communication for OLED display
+
+//DAC initializations
 dac.reset();
 dac.wake();
-dac.setVref(0,0);
+dac.setVref(0,0);   
 delay(10);
-dac.setGain(0,0);
+dac.setGain(0,0); 
 delay(10);
 dac.setPowerDown(1,1);
 
-  // Enable power for MCP342x (needed for FL100 shield only)
+// Enable power for MCP342x (needed for FL100 shield only)
   
-  // Reset devices
-  MCP342x::generalCallReset();
-  delay(1); // MC342x needs 300us to settle, wait 1ms
+// Reset devices
+MCP342x::generalCallReset();
+delay(1); // MC342x needs 300us to settle, wait 1ms
   
-  // Check device present
-  Wire.requestFrom(address, (uint8_t)1);
+// Check device present - OLED
+Wire.requestFrom(address, (uint8_t)1);
   if (!Wire.available()) {
     Serial.print("No device found at address ");
     Serial.println(address, HEX);
@@ -46,7 +80,7 @@ dac.setPowerDown(1,1);
   }
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3C for 128x32
-    Serial.println(F("SSD1306 allocation failed"));
+    Serial.println(F("SSD1306 allocation failed")); //Make sure global variables are lesser than 1024bytes to avoid this error.
     for(;;); // Don't proceed, loop forever
   }
 
@@ -55,6 +89,8 @@ dac.setPowerDown(1,1);
   
   // put your setup code here, to run once:
 
+pinMode(7, INPUT_PULLUP); //SW1 is D7
+pinMode(8, INPUT_PULLUP); //SW2 is D8
 }
 
 
@@ -63,11 +99,23 @@ float chgcurrent=0;
 float dchgcurrent=0;
 float temp=0;
 
-void loop() {
- dac.wake();
- dac.analogWrite(0,50); //dchg,chg
 
- voltage=readvoltage();
+
+
+
+//Main loop
+void loop() {
+dac.wake();
+
+buttonWait(7,8);    //This enables mode control using - SW1 connected to D7 and SW2 connected to D8
+
+voltage=readvoltage();
+ //If under-voltage, reset to idle state
+ if(voltage <= UNDER_VOLTAGE || voltage>= OVER_VOLTAGE)
+  {
+  idle_state();
+  }
+ 
  chgcurrent=readchgcurrent();
  dchgcurrent=readdchgcurrent();
  temp=readtemp();
@@ -92,16 +140,23 @@ void loop() {
   display.setCursor(0,30);  
   display.print("NTC(V)= ");           
   display.println(temp);
+
+  display.setCursor(0,39);  
+  display.print("Mode = ");           
+  display.println(mode);
+
   
 
-  display.setCursor(0,46);
+  display.setCursor(0,49);
   display.print("2019 Makermax Inc.");
-  display.setCursor(0,56);
+  display.setCursor(0,57);
   display.print("makermax.ca/boards");
   display.display();
   
   delay(200);
   // put your main code here, to run repeatedly:
+
+
 
 }
 
@@ -180,4 +235,147 @@ int readtemp()
   temp=temp*2.048;
 
   return temp;
+}
+
+
+
+
+
+//MODE logic implementation
+void buttonWait(int SW1, int SW2)
+{
+int buttonState1 = 0;
+int buttonState2 = 0;
+
+buttonState1 = digitalRead(SW1);
+buttonState2 = digitalRead(SW2);
+
+//Polling for Charge  
+ if(buttonState1== LOW)
+ {
+  if(!controlLock)
+  {
+  
+  delay(200);
+      if(buttonState2 == LOW)
+      {
+       delay(100);
+       mode++;
+       if(mode>=0 && mode <=2)
+       {
+       mode_change(mode);    
+       }
+       else
+       {
+       mode = 0;      
+       }
+      }else
+       {
+        if(mode == 1)
+        {
+        delay(500);
+        chg_curr -= 50;
+        delay(500);
+        charging(chg_curr);  
+        }
+        else if(mode == 2)
+        {
+        delay(500);
+        dchg_curr -= 50;
+        discharging(dchg_curr);
+        }
+       }
+  }
+ }else if(buttonState2 == LOW)
+  {
+   if(!controlLock)
+   {
+        if(mode == 1)
+        {
+        delay(500);
+        chg_curr += 50;
+        delay(500);
+        charging(chg_curr);  
+        }else if(mode == 2)
+           {
+           delay(500);
+           dchg_curr += 50;
+           discharging(dchg_curr);
+           }
+   }
+  }
+ 
+ else 
+ {
+  controlLock = false;
+ }
+
+}
+  
+//Switches to the appropriate function for the specified mode
+void mode_change(uint8_t mode){
+
+  if(mode == 0)
+    {
+     idle_state(); //idle
+    }
+  else if(mode == 1)
+    {
+     charging(CHARGE); //Charging
+    }
+    else if(mode == 2)
+    {
+     discharging(DISCHARGE); //Discharging
+    }
+    controlLock = true;
+}
+
+
+
+//Function to be called for charging
+//@param - currentVal(mA)
+void charging(float curr_val)
+{
+    if(curr_val>=0 && curr_val<=1200)
+    {
+    Serial.print(" Charge- ");
+    Serial.println(curr_val);
+    chg_curr = curr_val;
+    curr_val = curr_val/8.77;
+    dac.analogWrite(0,curr_val);
+    
+    }else
+     {
+     Serial.println("Enter a value between 0-120; 0->0mA | 120->1.15A");
+     curr_val = 0;
+     }
+  controlLock = true;
+  }
+
+  
+ //Neither charging nor discharging
+ void idle_state()
+ {
+  dac.analogWrite(0,0);
+ }
+
+
+
+//Function for discharging
+//@param - currentValue(mA)
+void discharging(float curr_val)
+{
+  if(curr_val>=0 && curr_val<=1200)
+  {
+    Serial.print("Discharge-");
+    Serial.println(curr_val);
+    dchg_curr = curr_val;
+    curr_val = curr_val/8.77;
+    dac.analogWrite(curr_val, 0);
+  }
+  else
+   {
+    Serial.println("Enter between 0-120");
+   }
+  controlLock = true;
 }
